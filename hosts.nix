e6ecdb0fd92026a510520c7eeee7167d663541e2
キロリス <kiroris@proton.me>
2025-10-02 14:19:16 +0300

    Create hosts.nix

---
+++ hosts.nix
@@ -0,0 +1,39 @@
+{
+  nixos = {
+    pcbox = {
+      username = "kiroris";
+      platform = "x86_64-linux";
+      stateVersion = "25.05";
+      isWorkstation = true;
+      wm = "sway";
+      theme = "nord";
+    };
+
+    nbox = {
+      username = "kiroris";
+      platform = "x86_64-linux";
+      stateVersion = "25.05";
+      isWorkstation = true;
+      wm = "sway";
+      theme = "nord";
+    };
+
+    rasp = {
+      username = "kiroris";
+      platform = "aarch64-linux";
+      stateVersion = "25.05";
+      isWorkstation = false;
+      theme = "nord";
+    };
+  };
+
+  darwin = {
+    macbox = {
+      username = "kiroris";
+      platform = "aarch64-darwin";
+      stateVersion = 5;
+      isWorkstation = true;
+      theme = "nord";
+    };
+  };
+}
